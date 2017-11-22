a5ae4424f022c37757683d563b7b9b943f479eae
dpmforensics <dpmforensics@gmail.com>
2017-11-22 18:46:36 -0500

    Remove unused regex lines.

--- pst_go_phish.py
+++ pst_go_phish.py
@@ -50,14 +50,6 @@ suspicious_messages = 0
 ignored_messages = 0
 no_body_messages = 0
 
-link_regex = re.compile(
-    r'^(?:http|ftp)s?://'  # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-    r'localhost|'  # localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
-    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
-    r'(?::\d+)?'  # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 def main(pst_file, output_dir, ig, threshold, links):
 	print("[+] Accessing {} PST file..".format(pst_file))
